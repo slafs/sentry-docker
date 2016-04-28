466809974fd6bfe03f953dff0e0b661fb94189cd
Mahesh Paolini-Subramanya <mahesh@dieswaytoofast.com>
2016-04-27 22:13:29 -0400

    Customize setup to support cielo's docker formats
    * Upgrade Sentry to 8.3.2
    * Clean up options to eliminate errors
    * Use standard dockerfile/jinja format

--- sentry_docker_conf.py
+++ sentry_docker_conf.py
@@ -49,15 +49,53 @@ def nydus_config(from_env_var):
         'hosts': _redis_hosts
     }
 
+def redis_cluster_config(from_env_var):
+    """
+    Generate a Nydus Redis configuration from an ENV variable of the form "server:port,server:port..."
+    Default to REDIS_HOST:REDIS_PORT if the ENV variable is not provided.
+    """
+    redis_servers_cast = lambda x: list(r.split(':') for r in x.split(','))
+    servers = config(from_env_var, default='{0}:{1}'.format(REDIS_HOST, REDIS_PORT), cast=redis_servers_cast)
+    _redis_hosts = {}
+
+    for r_index, r_host_pair in enumerate(servers):
+        _redis_hosts[r_index] = {'host': r_host_pair[0], 'port': int(r_host_pair[1])}
+
+    return {
+        'default': {
+            'hosts': _redis_hosts
+        }
+    }
+
 
 ############################
 # General Sentry options ##
 ############################
+
 SENTRY_OPTIONS = {
+    # If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
+    # Changing this value will result in all current sessions being invalidated
+    'system.secret-key': config('SECRET_KEY'),
+
     # You MUST configure the absolute URI root for Sentry:
     'system.url-prefix': config('SENTRY_URL_PREFIX'),
+
+    # For more information check Django's documentation:
+    #  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
     'system.admin-email': config('SENTRY_ADMIN_EMAIL', default='root@localhost'),
+    'mail.backend': config('SENTRY_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend'),
+    'mail.host': config('SENTRY_EMAIL_HOST', default='localhost'),
+    'mail.password': config('SENTRY_EMAIL_HOST_PASSWORD', default=''),
+    'mail.username': config('SENTRY_EMAIL_HOST_USER', default=''),
+    'mail.port': config('SENTRY_EMAIL_PORT', default=25, cast=int),
+    'mail.use-tls': config('SENTRY_EMAIL_USE_TLS', default=False, cast=bool),
+    # The email address to send on behalf of
+    'mail.from': config('SENTRY_SERVER_EMAIL', default='root@localhost'),
+
+    # Redis
+    'redis.clusters': redis_cluster_config('SENTRY_REDIS_BUFFERS')
 }
+
 
 ###########
 # Queue ##
@@ -94,7 +132,6 @@ SENTRY_USE_REDIS_BUFFERS = config('SENTRY_USE_REDIS_BUFFERS', default=False, cas
 
 if SENTRY_USE_REDIS_BUFFERS:
     SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
-    SENTRY_REDIS_OPTIONS = nydus_config('SENTRY_REDIS_BUFFERS')
     SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
 
 #######################
@@ -108,7 +145,7 @@ SENTRY_USE_REDIS_TSDB = config('SENTRY_USE_REDIS_TSDB', default=False, cast=bool
 
 if SENTRY_USE_REDIS_TSDB:
     SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
-    SENTRY_TSDB_OPTIONS = nydus_config('SENTRY_REDIS_TSDBS')
+
 
 ################
 # Web Server ##
@@ -125,40 +162,18 @@ SENTRY_WEB_OPTIONS = {
     'workers': config('SENTRY_WORKERS', default=3, cast=int),  # the number of gunicorn workers
     'limit_request_line': 0,  # required for raven-js
     'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
-    'errorlog' : os.path.join(DATA_DIR, 'gunicorn_error.log'),
-    'accesslog' : os.path.join(DATA_DIR, 'gunicorn_access.log'),
+    'errorlog' : os.path.join(DATA_DIR, 'sentry_web_error.log'),
+    'accesslog' : os.path.join(DATA_DIR, 'sentry_web_access.log'),
 }
 
 # allows JavaScript clients to submit cross-domain error reports. Useful for local development
 SENTRY_ALLOW_ORIGIN = config('SENTRY_ALLOW_ORIGIN', default=None)
-
-#################
-# Mail Server ##
-#################
-
-# For more information check Django's documentation:
-#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
-
-EMAIL_BACKEND = config('SENTRY_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-
-EMAIL_HOST = config('SENTRY_EMAIL_HOST', default='localhost')
-EMAIL_HOST_PASSWORD = config('SENTRY_EMAIL_HOST_PASSWORD', default='')
-EMAIL_HOST_USER = config('SENTRY_EMAIL_HOST_USER', default='')
-EMAIL_PORT = config('SENTRY_EMAIL_PORT', default=25, cast=int)
-EMAIL_USE_TLS = config('SENTRY_EMAIL_USE_TLS', default=False, cast=bool)
-
-# The email address to send on behalf of
-SERVER_EMAIL = config('SENTRY_SERVER_EMAIL', default='root@localhost')
 
 ###########
 # etc. ##
 ###########
 
 SENTRY_FEATURES['auth:register'] = config('SENTRY_ALLOW_REGISTRATION', default=False, cast=bool)
-
-# If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
-# Changing this value will result in all current sessions being invalidated
-SECRET_KEY = config('SECRET_KEY')
 
 # http://twitter.com/apps/new
 # It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
