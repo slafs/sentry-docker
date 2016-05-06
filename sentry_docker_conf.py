3b1385e69b66906f35c4a76091888671b2d789bb
Mahesh Paolini-Subramanya <mahesh@dieswaytoofast.com>
2016-05-06 16:55:43 -0400

    First pass at adding Google SSO
    * Add support for multiple email domains for login
    * Add support for external configuration of OAuth params

--- sentry_docker_conf.py
+++ sentry_docker_conf.py
@@ -300,3 +300,24 @@ if SENTRY_USE_REMOTE_USER:
         MIDDLEWARE_CLASSES += ('sentry_config.LAZY_CUSTOM_REMOTE_USER_MIDDLEWARE',)
     else:
         MIDDLEWARE_CLASSES += ('django.contrib.auth.middleware.RemoteUserMiddleware',)
+
+##############################
+# Plugins #
+##############################
+def get_domains(domains):
+    """
+    Change comma separated domains into a list
+    :param domains:
+    :return:
+    """
+    domains_list = []
+    if str is not None:
+        domains_list = domains.split(',')
+    return(domains_list)
+
+
+# Google SSO
+INSTALLED_APPS +=  ('sentry_sso_google',)
+GOOGLE_OAUTH2_CLIENT_ID = config('SENTRY_GOOGLE_OAUTH2_CLIENT_ID', default=None)
+GOOGLE_OAUTH2_CLIENT_SECRET = config('SENTRY_GOOGLE_OAUTH2_CLIENT_SECRET', default=None)
+GOOGLE_WHITE_LISTED_DOMAINS = get_domains(config('SENTRY_GOOGLE_WHITE_LISTED_DOMAINS', default=None))
